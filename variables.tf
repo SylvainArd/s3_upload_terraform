858faa90df4bfb493ace888e1b9960694ec515c9
Sylvain Ard <sylvain.ard@gmail.com>
2024-07-27 18:10:02 +0200

    first commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "The name of the SSH key to use"
+  type        = string
+}
+
+variable "public_key" {
+  description = "The SSH public key"
+  type        = string
+}
